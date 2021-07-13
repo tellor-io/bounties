d62de982273b649228372289d5b02a85fdf75546
Joshua Samaniego <joshuadsamaniego@gmail.com>
2021-07-13 17:28:45 -0600

    adds ContentTable.js

--- tellorbounties/src/App.js
+++ tellorbounties/src/App.js
@@ -1,9 +1,4 @@
-import React, { useState, useEffect } from "react";
-// Data Imports
-import dataContent from "./data/content.json";
-
-//Ant D
-// import { Button, Table } from "antd";
+import React from "react";
 
 //Component Imports
 import Header from "./components/Header.js";
@@ -11,23 +6,10 @@ import Welcome from "./components/Welcome.js";
 import BuildsTable from "./components/BuildsTable";
 import HacksTable from "./components/HacksTable";
 import DeeperResearchTable from "./components/DeeperResearchTable";
+import ContentTable from "./components/ContentTable";
 import Footer from "./components/Footer.js";
 
 const App = () => {
-  const [contentData, setContentData] = useState();
-
-  //   const contentUrl =
-  //     "https://api.sheety.co/ed9240fc3b351479d6da738838e4133d/tellorBountiesProgram/content";
-  //   fetch(contentUrl)
-  //     .then((response) => response.json())
-  //     .then((result) => {
-  //       setBountiesData(result.content);
-  //     });
-
-  useEffect(() => {
-    setContentData(dataContent.content);
-  }, []);
-
   return (
     <div className="App">
       <div className="App__Container">
@@ -36,23 +18,7 @@ const App = () => {
         <BuildsTable />
         <HacksTable />
         <DeeperResearchTable />
-        {/*
-        {contentData &&
-          contentData.map((data) => {
-            return (
-              <div key={data.id}>
-                <h2>{data.content ? data.content : null}</h2>
-                <h3>{data.tributes ? data.tributes : null}</h3>
-                <h4>{data.available ? data.available : null}</h4>
-                <div>
-                  <p>{data.description ? data.description : null}</p>
-                  <p>{data.skills ? data.skills : null}</p>
-                  <p>{data.notes ? data.notes : null}</p>
-                </div>
-              </div>
-            );
-          })}
-          })} */}
+        <ContentTable />
       </div>
       <Footer />
     </div>
